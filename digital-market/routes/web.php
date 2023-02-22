8cb6c7b470f2bce6ad92ce29b53982952459cf6a
Kutay Sütçü <kutay.sutcu@gmail.com>
2023-02-23 01:16:13 +0300

    Admin Dashboard and Admin Login Page Created

--- digital-market/routes/web.php
+++ digital-market/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Admin Routes
+
+Route::prefix('admin')->group(function(){
+    Route::get('/login', [AdminController::class, 'index'])->name('login_form');
+    Route::get('/login/owner', [AdminController::class, 'login'])->name('admin.login');
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+});
+
+//#Admin Routes
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
